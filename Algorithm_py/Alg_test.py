1340439c6f4c77cb0e13a9ada097dbc08763fe6e
26Sir <1141598581@qq.com>
2021-08-19 16:49:33 +0800

    首次提交

---
+++ Algorithm_py/Alg_test.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+
+arr = [1,4,7,3,12,3,34,56,23,67,22]
+
+def bubbleSort(arr):
+    for i in range(1,len(arr)):
+        for j in range(0,len(arr)-1):
+            if arr[j] > arr[i]:
+                arr[j],arr[j+1]=arr[j+1],arr[j]
+    return arr
